2d77351d344fda5777c4a94fa1e9bf84322ef47e
Ahingg <145777456+Ahingg@users.noreply.github.com>
2024-10-10 10:51:26 +0700

    Time: 46 ms (14.29%), Space: 11.5 MB (42.86%) - LeetHub

--- 1002-maximum-width-ramp/1002-maximum-width-ramp.c
+++ 1002-maximum-width-ramp/1002-maximum-width-ramp.c
@@ -1,20 +1,29 @@
-int maxWidthRamp(int* nums, int numsSize) {
-    int rightMax[numsSize] = {};
+int maxWidthRamp(int* nums,const int numsSize) {
+    int monoStack[numsSize] = {};
+    int stackTop = -1;
 
-    rightMax[numsSize-1] = nums[numsSize-1];
+    monoStack[0] = 0;
+    stackTop = 0;
 
-    for(int i = numsSize-2; i >= 0; i--){
-        rightMax[i] = rightMax[i+1] > nums[i] ? rightMax[i+1] : nums[i];
-    }
-
-    int left = 0, right = 0, max = 0;
-
-    while(right < numsSize){
-        while(left < right && nums[left] > rightMax[right]){
-            left++;
+    for(int i = 0; i < numsSize; i++){
+        if(nums[i] < nums[monoStack[stackTop]]){
+            monoStack[stackTop+1] = i;
+            stackTop++;
         }
-        max = max > right - left? max : right - left;
-        right++;
     }
+
+    int max = 0;
+    for(int i = numsSize-1; i >= 0; i--){
+        if(stackTop == -1){
+            break;
+        }
+
+        while(stackTop >= 0 && nums[i] >= nums[monoStack[stackTop]]){
+            const int len = i - monoStack[stackTop];
+            if(len > max) max = len;
+            stackTop--;
+        }
+    }
+
     return max;
 }
