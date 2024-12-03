c40192e8e7875f85f766e718c0108a602dfba465
Ahingg <145777456+Ahingg@users.noreply.github.com>
2024-12-03 14:50:03 +0700

    Time: 4 ms (75%), Space: 28.2 MB (16.67%) - LeetHub

--- 2232-adding-spaces-to-a-string/2232-adding-spaces-to-a-string.c
+++ 2232-adding-spaces-to-a-string/2232-adding-spaces-to-a-string.c
@@ -1,21 +1,19 @@
 char* addSpaces(char* s, int* spaces, int spacesSize) {
     int index = 0;
-    int strIndex = 0;
+
     int strLen = strlen(s);
     int totalLen = spacesSize + strLen;
     char *string = (char*)malloc((totalLen+1) * sizeof(char));
     
 
-    for(int i = 0; i < totalLen; i++){
+    for(int i = 0; i < strLen; i++){
         
-        if(index < spacesSize && strIndex == spaces[index]){
-            
-            string[i++] = ' ';
+        if(index < spacesSize && i == spaces[index]){
+            string[i + index] = ' ';
             index++;
-            
         }
         
-        string[i] = s[strIndex++]; 
+        string[i + index] = s[i]; 
     }
     string[totalLen] = '\0';
     return string;
