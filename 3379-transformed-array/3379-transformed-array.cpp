316a09f4fc6ce3487a34d1f3a922fb26004e1068
Ahingg <145777456+Ahingg@users.noreply.github.com>
2026-02-05 11:59:28 +0700

    Time: 4 ms (79.67%), Space: 25.9 MB (8.08%) - LeetHub

--- 3379-transformed-array/3379-transformed-array.cpp
+++ 3379-transformed-array/3379-transformed-array.cpp
@@ -2,17 +2,17 @@ class Solution {
 public:
     vector<int> constructTransformedArray(vector<int>& nums) {
         int n = nums.size();
-        vector<int> result(n);
+        vector<int> result;
         for(int i = 0; i < n; i++) {
-            int x = abs(nums[i])%n;
-            result[i] = nums[((nums[i] < 0) ? n + i - x : i+x)%n];
-
-            // if(x > 0) result.push_back(nums[(x+i)%n]);
-            // else if(x < 0) {
-            //     cout << n+(i+(x%n)) << " " << i+(x%n);
-            //     result.push_back(nums[((abs(x) > i) ? (n+(i+(x%n)))%n : (i+(x%n)%n))]);
-            // }
-            // else result.push_back(0);
+            // int x = abs(nums[i])%n;
+            // result[i] = nums[((nums[i] < 0) ? n + i - x : i+x)%n];
+            int x = nums[i];
+            if(x > 0) result.push_back(nums[(x+i)%n]);
+            else if(x < 0) {
+                // cout << n+(i+(x%n)) << " " << i+(x%n);
+                result.push_back(nums[((abs(x) > i) ? (n+(i+(x%n)))%n : (i+(x%n)%n))]);
+            }
+            else result.push_back(0);
 
         }
         return result;
