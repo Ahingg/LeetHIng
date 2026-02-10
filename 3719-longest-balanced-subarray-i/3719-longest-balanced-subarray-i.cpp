e7d3225e38a5b9418931c5522c53c6ace5598daf
Ahingg <145777456+Ahingg@users.noreply.github.com>
2026-02-10 23:43:12 +0700

    Time: 1641 ms (88.47%), Space: 427 MB (85.54%) - LeetHub

--- 3719-longest-balanced-subarray-i/3719-longest-balanced-subarray-i.cpp
+++ 3719-longest-balanced-subarray-i/3719-longest-balanced-subarray-i.cpp
@@ -1,19 +1,21 @@
 class Solution {
 public:
     int longestBalanced(vector<int>& nums) {
+        unordered_map<int, int> odd, even;
         int n = nums.size();
         int maxLength = 0;
-        for(int i = 0; i < n; i++){
-            unordered_map<int, int> odd;
-            unordered_map<int, int> even;
+        for(int i = 0; i < n-1; i++){
+            odd.clear(); even.clear();
 
-            for(int j = i; j < n; j++){
-                auto& target = (nums[j] & 1) ? odd : even;
-                target[nums[j]]++;
+            if(nums[i] & 1) odd[nums[i]]++;
+            else even[nums[i]]++;
+
+            for(int j = i+1; j < n; j++){
+                if(nums[j] & 1) odd[nums[j]]++;
+                else even[nums[j]]++;
 
                 if(odd.size() == even.size()) maxLength = max(maxLength, j-i+1);
             }
-
         }
 
         return maxLength;
