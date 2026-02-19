4d49c8a15a01f085c07c7de0bd1741e0a32bc45d
Ahingg <145777456+Ahingg@users.noreply.github.com>
2026-02-19 21:15:15 +0700

    Time: 0 ms (100%), Space: 13.1 MB (32.17%) - LeetHub

--- 0696-count-binary-substrings/0696-count-binary-substrings.cpp
+++ 0696-count-binary-substrings/0696-count-binary-substrings.cpp
@@ -1,33 +1,15 @@
 class Solution {
 public:
     int countBinarySubstrings(string s) {
-        int count1 = 0, count0 = 0;
-        if(s[0] - '0') count1++;
-        else count0++;
+        int count[2] = {0,0};
+        count[s[0] - '0']++;
         int total = 0;
         for(int i = 1; i < s.length(); i++) {
             int x = s[i] - '0';
             int y = (s[i-1] - '0');
-            if(x){
-                if(x == y) count1++;
-                else count1 = 1; 
-                if(count0 >= count1) {
-                    total++;
-                    cout << i << ": hit on " << count0 << " " << count1 <<  endl;
-                }
-            }
-            else {
-                if(x == y){
-                    count0++;
-                }
-                else {
-                    count0 = 1;
-                }
-                if(count0 <= count1) {
-                    total++;
-                    cout << i << ": hit on " << count0 << " " << count1 <<  endl;
-                }
-            }
+            if(x == y) count[x]++;
+            else count[x] = 1; 
+            if(count[!x] >= count[x]) total++;
         }
         return total;
     }
