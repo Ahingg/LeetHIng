2d61e90a71fddf85766dd25d96f471e1337763b9
Ahingg <145777456+Ahingg@users.noreply.github.com>
2026-02-23 13:58:22 +0700

    Time: 155 ms (80.62%), Space: 62.1 MB (82.25%) - LeetHub

--- 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.cpp
+++ 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.cpp
@@ -1,14 +1,28 @@
 class Solution {
 public:
     bool hasAllCodes(string s, int k) {
+        int maximumValue = pow(2,k) - 1;
         int len = s.length();
-        if(s.length() <= k) return false;
-        
-        unordered_set<string> list;
-        for(int i = 0; i + k <= len; i++){
-            list.insert(s.substr(i, k));
+        if(k >= len) return false;
+
+        unordered_map<int, int> list;
+
+        int currentValue = (s[0]-'0') & 1;
+        for(int i = 1; i < k; i++){
+            currentValue <<= 1;
+            currentValue += s[i] - '0';
         }
-        return list.size() == (1 << k);
+        list[currentValue] = 1;
+        // cout << currentValue << endl;
+        for(int i = 1; i + k <= len; i++){
+            currentValue <<= 1;
+            currentValue += s[i+k-1] -'0';
+            currentValue &= maximumValue;
+            list[currentValue] = 1;
+            // cout << i << ": " << currentValue << endl;
+        }
+        // cout << "size: " << list.size() << endl;
+        return list.size() == maximumValue + 1;
 
     }
 };
