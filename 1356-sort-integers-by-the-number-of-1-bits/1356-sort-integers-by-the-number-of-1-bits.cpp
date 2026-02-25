4a629a80d12750e155dc4474f04d323c02536973
Ahingg <145777456+Ahingg@users.noreply.github.com>
2026-02-25 13:35:02 +0700

    Time: 0 ms (100%), Space: 14.2 MB (79.86%) - LeetHub

--- 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.cpp
+++ 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.cpp
@@ -1,25 +1,14 @@
 class Solution {
 public:
-    int getBitCount(int x){
-        int count = 0;
-        while(x) {
-            if(x&1) count++;
-            x >>= 1;
-        }
-        return count;
+    static bool compare(int a, int b){
+        int countA = __builtin_popcount(a);
+        int countB = __builtin_popcount(b);
+        if(countA == countB) return a < b;
+        return countA < countB;
     }
     vector<int> sortByBits(vector<int>& arr) {
-        int len = arr.size();
-        for(int i = 0; i < len-1; i++){
-            for(int j = 0; j < len-i-1; j++){
-                int bit1 = getBitCount(arr[j]), bit2 = getBitCount(arr[j+1]);
-                if(bit1 > bit2 || (arr[j] >= arr[j+1] && bit1 == bit2)){
-                    int temp = arr[j];
-                    arr[j] = arr[j+1];
-                    arr[j+1] = temp;
-                }
-            }
-        }
+        sort(arr.begin(), arr.end( ), compare);
+        
         return arr;
     }
 };
