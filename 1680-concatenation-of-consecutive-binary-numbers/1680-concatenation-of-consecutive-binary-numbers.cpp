8b5a2b155659649d61602f5a9ddc44b83859220f
Ahingg <145777456+Ahingg@users.noreply.github.com>
2026-02-28 11:01:15 +0700

    Time: 23 ms (94.63%), Space: 8.1 MB (41.61%) - LeetHub

--- 1680-concatenation-of-consecutive-binary-numbers/1680-concatenation-of-consecutive-binary-numbers.cpp
+++ 1680-concatenation-of-consecutive-binary-numbers/1680-concatenation-of-consecutive-binary-numbers.cpp
@@ -2,21 +2,14 @@ class Solution {
 public:
     typedef long long ll;
     const ll MOD = 1e9 + 7;
-    int getBinaryLength(int x){
-        int len = 0;
-        while(x){
-            len++;
-            x >>= 1;
-        }
-        return len;
-    }
 
     int concatenatedBinary(int n) {
         if(n == 1) return 1;
         ll currentResult = 1;
+        int currLen = 1;
         for(int i = 2; i <= n; i++){
-            int len = getBinaryLength(i);
-            currentResult = ((currentResult << len) + i)%MOD;
+            if((i & i-1) == 0) currLen++;
+            currentResult = ((currentResult << currLen) + i)%MOD;
         }
         return currentResult;
     }
