b92eb99649a8c6926380927a63ac644647d174b9
Ahingg <145777456+Ahingg@users.noreply.github.com>
2026-03-08 21:08:43 +0700

    Time: 1598 ms (5.08%), Space: 173.7 MB (10.67%) - LeetHub

---
+++ 1980-find-unique-binary-string/1980-find-unique-binary-string.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    unordered_set<string> check;
+    string simulate(int n, string x){
+        if(n == 0) {
+            if(check.count(x)) return "";
+            return x;
+        }
+
+        string left = simulate(n-1, x + '0');
+        string right = simulate(n-1, x + '1');
+        if(left != "") return left;
+        return right;
+ 
+    }
+    string findDifferentBinaryString(vector<string>& nums) {
+        for(string x: nums) check.insert(x);
+        return simulate(nums[0].length(), "");
+    }
+};
