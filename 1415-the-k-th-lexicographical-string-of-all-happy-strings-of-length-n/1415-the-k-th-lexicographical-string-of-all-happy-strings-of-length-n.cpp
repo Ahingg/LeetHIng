6a0b80ebbf5139a1072b36ffa805bcff6ded71fb
Ahingg <145777456+Ahingg@users.noreply.github.com>
2026-03-14 15:37:13 +0700

    Time: 0 ms (100%), Space: 7.7 MB (96.74%) - LeetHub

--- 1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
+++ 1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
@@ -1,53 +1,61 @@
 class Solution {
 public:
 
-    int count = 0;
-    string bloom(int n, int k, char curr){
-        if(n == 1) {
-            count++;
-            string x = "";
-            if(count == k) return x + curr;
-            return "";            
-        }
-        char first, second;
-        switch(curr) {
-            case 'a':
-                first = 'b';
-                second = 'c';
-                break;
-            case 'b':
-                first = 'a';
-                second = 'c';
-                break;
-            case 'c':
-                first = 'a';
-                second = 'b';
-                break;
-        }
+    // int count = 0;
+    // string bloom(int n, int k, char curr){
+    //     if(n == 1) {
+    //         count++;
+    //         string x = "";
+    //         if(count == k) return x + curr;
+    //         return "";            
+    //     }
+    //     char first, second;
+    //     switch(curr) {
+    //         case 'a':
+    //             first = 'b';
+    //             second = 'c';
+    //             break;
+    //         case 'b':
+    //             first = 'a';
+    //             second = 'c';
+    //             break;
+    //         case 'c':
+    //             first = 'a';
+    //             second = 'b';
+    //             break;
+    //     }
+    //     string left = bloom(n-1, k, first);
+    //     if(left != "") return curr + left; 
+    //     string right = bloom(n-1, k, second);
+    //     if(right != "") return curr + right;
 
-        // cout << "Curr: " << curr << " Next: " << first << endl << endl;
-        // cout << count << " " << curr << endl;
-        string left = bloom(n-1, k, first);
-        if(left != "") return curr + left; 
-        string right = bloom(n-1, k, second);
-        if(right != "") return curr + right;
-
-        return "";
-    }
+    //     return "";
+    // }
     string getHappyString(int n, int k) {
-        if(n == 1){
-            char x = 'a' + k - 1;
-            string y = "";
-            if(k <= 3) return y + x;
-            else return "";
-        } 
-        string a = bloom(n, k, 'a');
-        if(a != "") return a;
-        string b = bloom(n, k, 'b');
-        if(b != "") return b;
-        string c = bloom(n, k, 'c');
-        if(c != "") return c;
-        // cout << a << " | " << b << " | " << c << endl;
-        return "";
+        int possible = (1 << (n-1));
+        if(k > 3*possible) return "";
+        string result = "";
+
+        k--;
+        int idx = k / possible;
+        char startPoint = (char)('a' + idx);
+        k %= possible;
+        result += startPoint;
+
+        for(int i = 1; i < n; i++){
+            possible >>= 1;
+
+            idx = k/possible;
+            char back = result.back();
+            char first = (back == 'a') ? 'b' : 'a';
+            char second = (back == 'c') ? 'b' : 'c';
+
+            if(idx == 0) result += first;
+            else result += second;
+
+            k %= possible;
+        }
+
+        return result;
     }
 };
