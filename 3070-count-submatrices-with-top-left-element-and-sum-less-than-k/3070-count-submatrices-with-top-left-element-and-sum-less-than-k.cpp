c1d372a2d50019c0629144c4f013566cb2d73445
Ahingg <145777456+Ahingg@users.noreply.github.com>
2026-03-18 13:07:13 +0700

    Time: 10 ms (82.02%), Space: 101 MB (45.51%) - LeetHub

--- 3070-count-submatrices-with-top-left-element-and-sum-less-than-k/3070-count-submatrices-with-top-left-element-and-sum-less-than-k.cpp
+++ 3070-count-submatrices-with-top-left-element-and-sum-less-than-k/3070-count-submatrices-with-top-left-element-and-sum-less-than-k.cpp
@@ -3,22 +3,23 @@ public:
     int countSubmatrices(vector<vector<int>>& grid, int k) {
         int m = grid.size();
         int n = grid[0].size();
+        if(grid[0][0] > k) return 0;
         vector<vector<int>> prefix(m, vector<int>(n, 0));
-        int result = 0;
-        for(int i = 0; i < m; i++){
-            for(int j = 0; j < n; j++){
-                
-                if(i == 0 && j == 0) prefix[i][j] = grid[i][j];
-                else if(i == 0) prefix[i][j] = grid[i][j] + prefix[i][j-1];
-                else if(j == 0) prefix[i][j] = grid[i][j] + prefix[i-1][j];
-                else {
-                    prefix[i][j] = grid[i][j] + prefix[i][j-1] + prefix[i-1][j] - prefix[i-1][j-1];
-                }
-                if(prefix[i][j] > k) {
-                    n = j+1;
-                    break;
-                }
-                result++;
+        int result = 1;
+        
+        prefix[0][0] = grid[0][0];
+        for(int i = 1; i < n; i++){
+            prefix[0][i] = grid[0][i] + prefix[0][i-1];
+            if(prefix[0][i] <= k) result++;
+        }
+        for(int i = 1; i < m; i++) {
+            prefix[i][0] = grid[i][0] + prefix[i-1][0];
+            if(prefix[i][0] <= k) result++;
+        }
+        for(int i = 1; i < m; i++){
+            for(int j = 1; j < n; j++){
+                prefix[i][j] = grid[i][j] + prefix[i][j-1] + prefix[i-1][j] - prefix[i-1][j-1];
+                if(prefix[i][j] <= k) result++;
             }
         }
         return result;
