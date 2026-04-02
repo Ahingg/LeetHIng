343d2e5dac4e67726473c21bc8ce03f054991cc9
Ahingg <145777456+Ahingg@users.noreply.github.com>
2026-04-02 17:28:35 +0700

    Time: 67 ms (90.64%), Space: 108.3 MB (88.51%) - LeetHub

---
+++ 3418-maximum-amount-of-money-robot-can-earn/3418-maximum-amount-of-money-robot-can-earn.cpp
@@ -0,0 +1,64 @@
+class Solution {
+public:
+    const int INF = -1e9;
+    int dp[501][501][3];
+    int maximumAmount(vector<vector<int>>& coins) {
+        int m = coins.size(), n = coins[0].size();
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                for(int k = 0; k < 3; k++){
+                    dp[i][j][k] = INF;
+                }
+            }
+        }
+
+        dp[0][0][0] = coins[0][0];
+        dp[0][0][1] = coins[0][0] < 0 ? 0 : INF;
+
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                if(i == 0 && j == 0) continue;
+                if(i == 0) {
+                    // take from left only
+                    dp[i][j][0] = dp[i][j-1][0] + coins[i][j];
+                    if(coins[i][j] >= 0) {
+                        dp[i][j][1] = dp[i][j-1][1] == INF ? INF : dp[i][j-1][1] + coins[i][j];
+                        dp[i][j][2] = dp[i][j-1][2] == INF ? INF : dp[i][j-1][2] + coins[i][j];
+                    }
+                    else {
+                        dp[i][j][1] = max(dp[i][j-1][0], dp[i][j-1][1] + coins[i][j]);
+                        dp[i][j][2] = max(dp[i][j-1][1], dp[i][j-1][2] + coins[i][j]);
+                    }
+                }
+                else if(j == 0) {
+                    dp[i][j][0] = dp[i-1][j][0] + coins[i][j];
+                    if(coins[i][j] >= 0) {
+                        dp[i][j][1] = dp[i-1][j][1] == INF ? INF : dp[i-1][j][1] + coins[i][j];
+                        dp[i][j][2] = dp[i-1][j][2] == INF ? INF : dp[i-1][j][2] + coins[i][j];
+                    }
+                    else {
+                        dp[i][j][1] = max(dp[i-1][j][0], dp[i-1][j][1] + coins[i][j]);
+                        dp[i][j][2] = max(dp[i-1][j][1], dp[i-1][j][2] + coins[i][j]);
+                    }
+                }
+                else {
+                    // take max from up and left
+                    dp[i][j][0] = max(dp[i-1][j][0] + coins[i][j], dp[i][j-1][0] + coins[i][j]);
+                    if(coins[i][j] >= 0) {
+                        dp[i][j][1] = dp[i-1][j][1] == INF ? INF : dp[i-1][j][1] + coins[i][j];
+                        dp[i][j][1] = max(dp[i][j][1], dp[i][j-1][1] == INF ? INF : dp[i][j-1][1] + coins[i][j]);
+                        dp[i][j][2] = dp[i-1][j][2] == INF ? INF : dp[i-1][j][2] + coins[i][j];
+                        dp[i][j][2] = max(dp[i][j][2], dp[i][j-1][2] == INF ? INF : dp[i][j-1][2] + coins[i][j]);
+                    }
+                    else {
+                        dp[i][j][1] = max(max(dp[i-1][j][0], dp[i-1][j][1] + coins[i][j]), max(dp[i][j-1][0], dp[i][j-1][1] + coins[i][j]));
+                        dp[i][j][2] = max(max(dp[i-1][j][1], dp[i-1][j][2] + coins[i][j]), max(dp[i][j-1][1], dp[i][j-1][2] + coins[i][j]));
+                    }
+                }
+                // cout << dp[i][j][0] << " " << dp[i][j][1] << " " << dp[i][j][2] << endl;
+            }
+            // cout << endl;
+        }
+        return max(dp[m-1][n-1][0], max(dp[m-1][n-1][1], dp[m-1][n-1][2]));
+    }
+};
