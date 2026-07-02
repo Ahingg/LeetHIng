2f2efdeb99e5161b80217efe02210037ca768f36
Ahingg <145777456+Ahingg@users.noreply.github.com>
2026-07-02 23:06:24 +0700

    Time: 24 ms (46.28%), Space: 95.7 MB (54.52%) - LeetHub

---
+++ 3739-count-subarrays-with-majority-element-ii/3739-count-subarrays-with-majority-element-ii.cpp
@@ -0,0 +1,48 @@
+class FenwickTree {
+public:
+    int size;
+    vector<int> tree;
+
+    FenwickTree(int n) {
+        size = n;
+        tree.assign(n + 1, 0);
+    }
+
+    void update(int idx, int delta) {
+        for(; idx <= size; idx += idx & -idx) tree[idx] += delta;
+    }
+
+    int query(int idx) {
+        int sum = 0;
+        for(; idx > 0; idx -= idx & -idx) sum += tree[idx];
+        return sum;
+    }
+};
+
+class Solution {
+public:
+    long long countMajoritySubarrays(vector<int>& nums, int target) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        int n = nums.size();
+
+        FenwickTree b((2 * n) + 2);
+        int offset = n + 1;
+
+        long long total = 0;
+        int currPref = 0;
+
+        b.update(currPref + offset, 1);
+
+        for(int x: nums) {
+            if(x == target) currPref++;
+            else currPref--;
+
+            long long valid = b.query(currPref + offset - 1);
+            total += valid;
+
+            b.update(currPref + offset, 1);
+        }
+        return total;
+    }
+};
