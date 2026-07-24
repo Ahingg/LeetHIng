e9c41c81c716287a3dd2c7eb1bd64f3522f16b62
Ahingg <145777456+Ahingg@users.noreply.github.com>
2026-07-24 07:56:05 +0700

    Time: 939 ms (58.99%), Space: 35.4 MB (80.9%) - LeetHub

--- 3514-number-of-unique-xor-triplets-ii/3514-number-of-unique-xor-triplets-ii.cpp
+++ 3514-number-of-unique-xor-triplets-ii/3514-number-of-unique-xor-triplets-ii.cpp
@@ -2,16 +2,16 @@ class Solution {
 public:
     int uniqueXorTriplets(vector<int>& nums) {
         int n = nums.size();
-        vector<bool> xo(4096, false);
+        vector<bool> xo(2049, false);
         for(int j = 0; j < n; j++) {
             for(int k = j; k < n; k++) {
                 xo[nums[j] ^ nums[k]] = true;
             }
         }
-        vector<bool> ans(4096, false);
+        vector<bool> ans(2049, false);
         int uniq = 0;
         for(int i = 0; i < n; i++) {
-            for(int a = 0; a < 4096; a++) {
+            for(int a = 0; a < 2049; a++) {
                 if(xo[a]) {
                     int idx = a ^ nums[i];
                     if(ans[idx]) continue;
