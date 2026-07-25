56dd16405f951520174e9ead98929d508fe91ca9
Ahingg <145777456+Ahingg@users.noreply.github.com>
2026-07-25 14:55:12 +0700

    Time: 0 ms (100%), Space: 8.6 MB (74.03%) - LeetHub

---
+++ 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int maxProduct(int n) {
+        int a = -1;
+        int b = -1;
+        while(n) {
+            int digit = n%10;
+            if(digit > a) {
+                b = a;
+                a = digit;
+            }
+            else if(digit > b) {
+                b = digit;
+            }
+            n /= 10;
+        }
+        return a * b;
+    }
+};
