7156454de68c50146d046e39ec6b5684083cbb2a
Ahingg <145777456+Ahingg@users.noreply.github.com>
2026-07-28 08:08:02 +0700

    Time: 23 ms (82.86%), Space: 54.3 MB (88.13%) - LeetHub

--- 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.cpp
+++ 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.cpp
@@ -4,21 +4,33 @@ public:
         int len = s.length();
 
         vector<int> cnt(26, 0);
-        for(const char c: s) cnt[c - 'a']++;
-        string result = "";
-        string result2 = "";
-        int oddIdx = -1;
+        for(int i = 0; i < len/2; i++) cnt[s[i] - 'a']++;
+
+        int idx = 0;
         for(int i = 0; i < 26; i++) {
-            if(!cnt[i]) continue;
-            if(cnt[i] & 1) oddIdx = i;
-            for(int j = 0; j < (cnt[i]/2); j++) {
-                result += ('a' + i);
-                result2 += ('a' + i);
+            while(cnt[i]-- > 0) {
+                s[idx++] = ('a' + i);
             }
         }
-        reverse(result2.begin(), result2.end());
-        if(oddIdx != -1) result += ('a' + oddIdx);
-        result += result2;
-        return result;
+
+        for(int i = 0; i < len/2; i++) {
+            s[len-i-1] = s[i];
+        } 
+        return s;
+        // string result = "";
+        // string result2 = "";
+        // int oddIdx = -1;
+        // for(int i = 0; i < 26; i++) {
+        //     if(!cnt[i]) continue;
+        //     if(cnt[i] & 1) oddIdx = i;
+        //     for(int j = 0; j < (cnt[i]/2); j++) {
+        //         result += ('a' + i);
+        //         result2 += ('a' + i);
+        //     }
+        // }
+        // reverse(result2.begin(), result2.end());
+        // if(oddIdx != -1) result += ('a' + oddIdx);
+        // result += result2;
+        // return result;
     }
 };
