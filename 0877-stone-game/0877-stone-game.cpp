e875d568b01d5af87ea8b5728c625331c958a18b
Ahingg <145777456+Ahingg@users.noreply.github.com>
2026-08-02 14:49:53 +0700

    Time: 15 ms (20.49%), Space: 28.4 MB (5.55%) - LeetHub

---
+++ 0877-stone-game/0877-stone-game.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    typedef long long ll;
+    bool stoneGame(vector<int>& nums) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+
+        int n = nums.size();
+        vector<vector<ll>> dp(n+1, vector<ll>(n+1, 0));
+        ll sum = 0;
+        for(int i = 0; i < n; i++) {
+            dp[i][i] = nums[i];
+            sum += nums[i];
+        } 
+
+        for(int k = 2; k <= n; k++) {
+            for(int i = 0; i < n - k + 1; i++) {
+                int j = i + k - 1;
+                dp[i][j] = max(nums[i] - dp[i+1][j], nums[j] - dp[i][j-1]);
+            }
+        }
+        // cout << dp[0][n-1];
+        return dp[0][n-1] >= 0;
+    }
+};
