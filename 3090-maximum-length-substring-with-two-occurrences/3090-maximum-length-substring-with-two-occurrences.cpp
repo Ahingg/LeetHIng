fdd379fc9e5fa1d74e8d94cc3a1fcb36eb67b39a
Ahingg <145777456+Ahingg@users.noreply.github.com>
2026-08-14 07:51:02 +0700

    Time: 0 ms (100%), Space: 9.1 MB (87.78%) - LeetHub

--- 3090-maximum-length-substring-with-two-occurrences/3090-maximum-length-substring-with-two-occurrences.cpp
+++ 3090-maximum-length-substring-with-two-occurrences/3090-maximum-length-substring-with-two-occurrences.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
     int maximumLengthSubstring(string s) {
-        int n =  s.length();
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL); cout.tie(NULL);
 
-        unordered_map<int, int> freq;
+        vector<int> freq(26, 0);
         int i = 0;
         int maxLen = 0;
-        for(int j = 0; j < n; j++) {
-            int idx = s[j] - 'a';
-            freq[idx]++;
+        for(int j = 0; j < s.length(); j++) {
+            freq[s[j] - 'a']++;
 
-            while(freq[idx] > 2) {
+            while(freq[s[j] - 'a'] > 2) {
                 freq[s[i++] - 'a']--;
             }
             maxLen = max(maxLen, j-i+1);
